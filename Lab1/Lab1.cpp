13c7b6cd02f5193925d82a722ca33b146dadf8e3
zigzar <zigzariz@mail.ru>
2021-05-24 02:02:33 +0300

    Lab 1 with "returning" value

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+typedef struct args_tag {
+	bool isActive;
+	int value;
+} args_t;
 
-void* threadFunc1(void* flag)
+void* threadFunc1(void* args)
 {
-	while(*((bool*)flag))
+	args_t* pArgs = (args_t*)args;
+	while(pArgs->isActive)
 	{
 		std::cout << "1 ";
 		std::cout.flush();
@@ -13,9 +18,10 @@ void* threadFunc1(void* flag)
 	return 0;
 }
 
-void* threadFunc2(void* flag)
+void* threadFunc2(void* args)
 {
-	while(*((bool*)flag))
+	args_t* pArgs = (args_t*)args;
+	while(pArgs->isActive)
 	{
 		std::cout << "2 ";
 		std::cout.flush();
@@ -24,21 +30,29 @@ void* threadFunc2(void* flag)
 	return 0;
 }
 
+void* aborterFunc(void* args)
+{
+	args_t* pArgs = (args_t*)args;
+	std::cin.get();
+	pArgs->isActive = false;
+}
 
 int main()
 {
-	bool isActive1 = true;
-	bool isActive2 = true;
-	int statusPtr1;
-	int statusPtr2;
-	pthread_t th1, th2;
+	args_t arg;
+	arg.isActive = true;
+	pthread_t th1, th2, aborter;
 	std::cout << "Press Enter to stop program:" << std::endl;
-	pthread_create(&th1, NULL, threadFunc1, &isActive1);
-	pthread_create(&th2, NULL, threadFunc2, &isActive2);
-	std::cin.get();
-	isActive1 = false;
-	isActive2 = false;
-	pthread_join(th1, (void**)&statusPtr1);
-	pthread_join(th2, (void**)&statusPtr1);
+	pthread_create(&th1, NULL, threadFunc1,  (void*)&arg);
+	pthread_create(&th2, NULL, threadFunc2,  (void*)&arg);
+	pthread_create(&aborter, NULL, aborterFunc,  (void*)&arg);
+	while (arg.isActive)
+	{
+		std::cout << arg.value << " ";
+		std::cout.flush();
+	}
+	pthread_join(th1, NULL);
+	pthread_join(th2, NULL);
+	pthread_join(aborter, NULL);
 	return 0;
 }
