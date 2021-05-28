179402ff5c23e53319ba9214c5b9d65ebe536522
zigzar <zigzariz@mail.ru>
2021-05-28 20:34:57 +0300

    Add Lab2_2 with sem_trywait()

--- Lab2/Lab2_1.cpp
+++ Lab2/Lab2_1.cpp
@@ -7,47 +7,47 @@ sem_t semaphore;
 
 const int NUM_PRINT = 3;
 
-void sleep(int ms) {
-    timespec tp;
-    tp.tv_sec = ms / 1000;
-    tp.tv_nsec = (ms % 1000) * 1000000;
-    nanosleep(&tp, NULL);
+void sleep(int ms)
+{
+	timespec tp;
+	tp.tv_sec = ms / 1000;
+	tp.tv_nsec = (ms % 1000) * 1000000;
+	nanosleep(&tp, NULL);
 }
 
-void* threadFunc1(void* flag)
+void *threadFunc1(void *flag)
 {
-	while(*((bool*)flag))
+	while (*((bool *)flag))
 	{
-        sem_wait(&semaphore);
-        for (int i = 0; i < NUM_PRINT; i++)
-        {
-            std::cout << "1 ";
-		    std::cout.flush();
-		    sleep(100);
-        }
-        sem_post(&semaphore);
-        sleep(500);
+		sem_wait(&semaphore);
+		for (int i = 0; i < NUM_PRINT; i++)
+		{
+			std::cout << "1 ";
+			std::cout.flush();
+			sleep(100);
+		}
+		sem_post(&semaphore);
+		sleep(500);
 	}
 	return 0;
 }
 
-void* threadFunc2(void* flag)
+void *threadFunc2(void *flag)
 {
-	while(*((bool*)flag))
+	while (*((bool *)flag))
 	{
-        sem_wait(&semaphore);
-        for (int i = 0; i < NUM_PRINT; i++)
-        {
-            std::cout << "2 ";
-		    std::cout.flush();
-		    sleep(100);
-        }
-        sem_post(&semaphore);
-        sleep(300);
+		sem_wait(&semaphore);
+		for (int i = 0; i < NUM_PRINT; i++)
+		{
+			std::cout << "2 ";
+			std::cout.flush();
+			sleep(100);
+		}
+		sem_post(&semaphore);
+		sleep(300);
 	}
 	return 0;
 }
-
 
 int main()
 {
@@ -57,7 +57,7 @@ int main()
 	int statusPtr2;
 	pthread_t th1, th2;
 
-    sem_init(&semaphore, 0, 1);
+	sem_init(&semaphore, 0, 1);
 	std::cout << "Press Enter to stop program:" << std::endl;
 	pthread_create(&th1, NULL, threadFunc1, &isActive1);
 	pthread_create(&th2, NULL, threadFunc2, &isActive2);
@@ -66,10 +66,10 @@ int main()
 	isActive1 = false;
 	isActive2 = false;
 
-	pthread_join(th1, (void**)&statusPtr1);
-	pthread_join(th2, (void**)&statusPtr1);
+	pthread_join(th1, (void **)&statusPtr1);
+	pthread_join(th2, (void **)&statusPtr1);
 
-    sem_destroy(&semaphore);
+	sem_destroy(&semaphore);
 
 	return 0;
 }
